8355ab9314876a266b2d568281dde25388a8d689
kiennguyen2403 <kientqk2403@gmail.com>
2024-11-05 21:32:53 +1100

    temporary assume that bucket is public

--- scrutiny_ai/app/confirm-invite/page.tsx
+++ scrutiny_ai/app/confirm-invite/page.tsx
@@ -160,7 +160,7 @@ const Page = () => {
                     </h4>
                   </div>
                 </div>
-
+                {meeting.description && (
                 <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
                   <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500 ml-0.5" />
                   <div className="space-y-1">
@@ -169,6 +169,7 @@ const Page = () => {
                     </h4>
                   </div>
                 </div>
+                )}
 
                 <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0 items-center">
                   <CalendarIcon className="mr-1 h-3 w-3 text-gray-500" />
